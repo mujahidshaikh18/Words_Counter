3ccca62b07981784b488ac2e1f477b085797c326
mujahidshaikh18 <smujahid995@gmail.com>
2024-04-22 20:43:35 +0500

    adding words counter project

---
+++ index.js
@@ -0,0 +1,18 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+console.log(chalk.bold.cyanBright("\n \t\tCODE WITH MUJAHID: WORD COUNTER"));
+console.log(chalk.cyanBright("=".repeat(60)));
+const answers = await inquirer.prompt([
+    {
+        name: "sentence",
+        type: "input",
+        message: chalk.gray("Enter your sentence:")
+    }
+]);
+const words = answers.sentence.trim().split(" ");
+console.log(chalk.cyanBright("=".repeat(60)));
+console.log(chalk.bold.redBright("- Sentence Words:"));
+console.log(words);
+console.log(chalk.bold.magenta(`\n - Your sentence words count is ${chalk.bold.redBright(words.length)}`));
+console.log(chalk.cyanBright("=".repeat(60)));
